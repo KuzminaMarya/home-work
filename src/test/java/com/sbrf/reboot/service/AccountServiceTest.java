af6ac397f56283dda0a5ba7e64f575bb77fae4ba
KuzminaMarya <kuzmina.marya.valerevna@yandex.ru>
2021-12-16 11:28:30 +0300

    Кузьмина Мария Д.з по урокам 10 и 11

--- src/test/java/com/sbrf/reboot/service/AccountServiceTest.java
+++ src/test/java/com/sbrf/reboot/service/AccountServiceTest.java
@@ -1,20 +1,15 @@
 package com.sbrf.reboot.service;
 
-import com.sbrf.reboot.Account;
-import com.sbrf.reboot.AccountRepository;
-import com.sbrf.reboot.AccountService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 class AccountServiceTest {
@@ -32,22 +27,22 @@ class AccountServiceTest {
     }
 
     @Test
-    void bookExist() throws IOException {
+    void bookExist(){
         Account account = new Account("ACC1234NUM");
         Set<Account> accounts = new HashSet();
         accounts.add(account);
 
-        Mockito.when(accountRepository.getAllAccountsByClientId(1L)).thenReturn(accounts);
+        when(accountRepository.getAllAccountsByClientId(1L)).thenReturn(accounts);
 
         assertTrue(accountService.isAccountExist(1L, account));
     }
 
     @Test
-    void bookNotExist() throws IOException {
+    void bookNotExist(){
         Set<Account> accounts = new HashSet();
         accounts.add(new Account("ACC1234NUM"));
 
-        Mockito.when(accountRepository.getAllAccountsByClientId(1L)).thenReturn(accounts);
+        when(accountRepository.getAllAccountsByClientId(1L)).thenReturn(accounts);
 
         assertFalse(accountService.isAccountExist(1L, new Account("ACC456NUM")));
     }
