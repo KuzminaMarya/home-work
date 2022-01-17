c8e422e33284c96ff851538535d79c893d6b6f34
KuzminaMarya <kuzmina.marya.valerevna@yandex.ru>
2022-01-17 11:44:11 +0300

    Кузьмина Мария. Домашнее задание № 14

--- src/main/java/com/sbrf/reboot/MainReport.java
+++ src/main/java/com/sbrf/reboot/MainReport.java
@@ -8,34 +8,30 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.*;
 
 public class MainReport {
-    static public int getTotalsWithCompletableFuture(Stream<Customer> streamCustomer) throws ExecutionException, InterruptedException {
+
+    static public int getTotalsWithCompletableFuture(Stream<Customer> streamCustomer) throws InterruptedException, ExecutionException {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(()
                 -> {
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                throw new IllegalStateException(e);
-            }
-            int sumBalance = 0;
             Set<Customer> customers = streamCustomer.filter(customer -> customer.getAge() >= 18 && customer.getAge() <= 30).collect(Collectors.toSet());
-            Iterator<Customer> iterator = customers.iterator();
-            while (iterator.hasNext()) {
-                Stream<AccountC> streamAccount = iterator.next().getAccount().stream();
+            return customers;
+        }).thenApplyAsync(result-> {
+            //Выполняется в другом потоке, взятом из ForkJoinPool.commonPool()
+            int sumBalance = 0;
+            for (Customer customer : result) {
+                Stream<AccountC> streamAccount = customer.getAccount().stream();
                 sumBalance += streamAccount.filter(accountC -> accountC.getDateCreated().isAfter(LocalDate.of(2021, 7, 1)) && accountC.getDateCreated().isBefore(LocalDate.of(2021, 8, 1)) && accountC.getCurrency().equals("RUB")).mapToInt(value -> value.getBalance()).sum();
             }
-            System.out.println("Результат получен ассинхронно");
+            System.out.println("Выполнено в другом потоке, взятом из ForkJoinPool.commonPool()");
             return sumBalance;
         });
-
         return future.get();
     }
 
-    static public int getTotalsWithReact(Stream<Customer> streamCustomer) throws InterruptedException {
+   static public int getTotalsWithReact(Stream<Customer> streamCustomer) throws InterruptedException {
         int sumBalance = 0;
         Set<Customer> customers = streamCustomer.filter(customer -> customer.getAge() >= 18 && customer.getAge() <= 30).collect(Collectors.toSet());
-        Iterator<Customer> iterator = customers.iterator();
-        while (iterator.hasNext()) {
-            Stream<AccountC> streamAccount = iterator.next().getAccount().stream();
+        for(Customer customer:customers){
+            Stream<AccountC> streamAccount = customer.getAccount().stream();
             sumBalance += streamAccount.filter(accountC -> accountC.getDateCreated().isAfter(LocalDate.of(2021, 7, 1)) && accountC.getDateCreated().isBefore(LocalDate.of(2021, 8, 1)) && accountC.getCurrency().equals("RUB")).mapToInt(value -> value.getBalance()).sum();
         }
         return sumBalance;
